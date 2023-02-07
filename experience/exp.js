4b5d67a5e34e5d6c47f6cd8e3b72227ec28bbea5
AmiranIvanidz3 <amiran.ivanidz3@gmail.com>
2023-02-07 14:31:11 -0800

    Create_Some_Useful_Variables_In_Exp.js

--- experience/exp.js
+++ experience/exp.js
@@ -1,12 +1,9 @@
+//variables-storage
 const name_span = document.getElementById('name-span')
 const lastName_span = document.getElementById('lastName-span')
 const mail_span_right = document.querySelector(".mail-span-right")
 const about = document.querySelector("#aboutMe")
 const mobile = document.querySelector(".mobile-span-right")
-
-
-
-
 name_span.innerText = sessionStorage.getItem('name')
 lastName_span.innerText = sessionStorage.getItem('lName')
 mail_span_right.innerText = sessionStorage.getItem('mail')
@@ -14,6 +11,17 @@ mobile.innerText = sessionStorage.getItem('phone')
 if(sessionStorage.getItem('about') !== ""){
     about.innerText = sessionStorage.getItem('about')
     document.querySelector(".aboutMe-span").style.opacity = "1"
-
-
 }
+
+
+
+//useful variables
+const tanamdeboba_input = document.getElementById("tanamdeboba-input")
+const employe_input = document.getElementById('employe-input')
+const start_date_input = document.getElementById("start-date-input")
+const end_date_input = document.getElementById("end-date-input")
+const describe_input = document.getElementById("describe-input")
+
+
+
+
