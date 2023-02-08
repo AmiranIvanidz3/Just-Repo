f935ad84d2eceba9c61440e96ac0fbe594c212dd
AmiranIvanidz3 <amiran.ivanidz3@gmail.com>
2023-02-08 03:00:31 -0800

    Experience_Page_Date_Validatation

--- experience/exp.js
+++ experience/exp.js
@@ -27,7 +27,28 @@ const employe_input = document.getElementById('employe-input')
 const start_date_input = document.getElementById("start-date-input")
 const end_date_input = document.getElementById("end-date-input")
 const describe_input = document.getElementById("describe-input")
+const form = document.getElementById("experience-form")
 
 
 
+
+//start date and end date validation 
+
+function validateDates() {
+    var startDate = new Date(document.querySelector("#start-date-input").value);
+    var endDate = new Date(document.querySelector("#end-date-input").value);
+    var status;
+    if (startDate > endDate) {
+      alert("End date must be after start date.");
+      status = false
+     
+
+    } else {
+      alert("Dates are valid.");
+      status = true
+    }
+    return status
+  }
+
+ 
 
